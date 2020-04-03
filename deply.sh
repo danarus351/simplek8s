d038a288bd3aee490fb105782288a44d28cae003
danarus351 <danarus.d@gmail.com>
2020-04-04 02:05:27 +0300

    added deployment

---
+++ deply.sh
@@ -0,0 +1,16 @@
+docker build -t danarus/multi_client:latest -t danarus/multi_client:$git_SHA -f ./client/Dockerfile ./client
+docker build -t danarus/multi_server:latest -t danarus/multi_server:$git_SHA -f ./server/Dockerfile ./server
+docker build -t danarus/multi_worker:latest -t danarus/multi_worker:$git_SHA -f ./multi_worker/Dockerfile ./docker 
+
+docker push danarus/multi_client:latest
+docker push danarus/multi_server:latest
+docker push danarus/multi_worker:latest
+
+docker push danarus/multi_client:$git_SHA
+docker push danarus/multi_server:$git_SHA
+docker push danarus/multi_worker:$git_SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=danarus/multi_server:$git_SHA
+kubectl set image deployments/client-deployment client=danarus/multi_client:$git_SHA
+kubectl set image deployments/worker-deployment worker=danarus/multi_worker:$git_SHA
